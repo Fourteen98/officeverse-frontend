e88a1a3375968258521d22fb90d05ecb0cde7467
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-30 19:21:15 +0000

    remove dark mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
+import Reserve from './pages/Reserve';
+import MyReservations from './pages/MyReservations';
+import './index.css';
 
 function App() {
   return (
@@ -8,6 +11,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/reserve" element={<Reserve />} />
+        <Route path="/my-reservations" element={<MyReservations />} />
       </Routes>
     </Router>
   );
