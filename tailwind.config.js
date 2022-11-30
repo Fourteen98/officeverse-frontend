451fc5c1c80051625abd90c64431c8e6d55ae5cc
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-30 10:02:21 +0000

    init tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    './public/*.html'
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-        nova: ['Proxima Nova', ...defaultTheme.fontFamily.sans]
+        sans: ['Proxima Nova', 'sans-serif'],
       },
       colors: {
         'main-splash': '#4970bf',
-        'main': '#d9cec1',
+        'main-': '#d9cec1',
         'main-second': '#bf7d65',
         'main-third': '#bf0404',
-        'main-fourth': '#f23838'
+        'main-fourth': '#f23838',
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+  plugins: [],
+};
