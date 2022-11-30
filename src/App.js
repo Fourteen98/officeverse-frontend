b66fe590805337cbc8835d28e9c05f13be9185f2
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-30 16:26:01 +0000

    add basic navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <p className="text-3xl">Testing tailwind styles</p>
-      <Home />
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
