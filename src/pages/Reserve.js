4b177ae88dc32465d7a8e296d3febcbc9cd73578
Diego Yon <diegoyon@outlook.com>
2022-12-01 17:13:18 -0600

    Finish implementing adding services and peripherals in the reservation form

--- src/pages/Reserve.js
+++ src/pages/Reserve.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
@@ -7,12 +7,52 @@ import { createReservation, fetchReservations } from '../redux/reservations/rese
 
 export default function Reserve() {
   const officeList = useSelector((state) => state.offices.offices);
+
   const servicesList = useSelector((state) => state.services.services);
+
   const peripheralsList = useSelector((state) => state.peripherals.peripherals);
 
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
-  const [office, setOffice] = useState();
+  const [office, setOffice] = useState(1);
+
+  const [checkedStateServices, setCheckedStateServices] = useState(new Array(9).fill(false));
+  const [arrayServices, setArrayServices] = useState([]);
+
+  const [checkedStatePeripherals, setCheckedStatePeripherals] = useState(new Array(9).fill(false));
+  const [arrayPeripherals, setArrayPeripherals] = useState([]);
+
+  useEffect(() => {
+    const arr = checkedStateServices.map((element, index) => {
+      if (element) {
+        return index + 1;
+      }
+      return null;
+    });
+    setArrayServices(arr.filter(Number));
+  }, [checkedStateServices]);
+
+  useEffect(() => {
+    const arr = checkedStatePeripherals.map((element, index) => {
+      if (element) {
+        return index + 1;
+      }
+      return null;
+    });
+    setArrayPeripherals(arr.filter(Number));
+  }, [checkedStatePeripherals]);
+
+  const handleOnChangeServices = (position) => {
+    const updatedCheckedStateServices = checkedStateServices.map((item, index) => (
+      index === position ? !item : item));
+    setCheckedStateServices(updatedCheckedStateServices);
+  };
+
+  const handleOnChangePeripherals = (position) => {
+    const updatedCheckedStatePeripherals = checkedStatePeripherals.map((item, index) => (
+      index === position ? !item : item));
+    setCheckedStatePeripherals(updatedCheckedStatePeripherals);
+  };
 
   const dispatch = useDispatch();
 
@@ -21,12 +61,16 @@ export default function Reserve() {
       start_date: startDate,
       end_date: endDate,
       office_id: office,
+      service_ids: arrayServices,
+      peripheral_ids: arrayPeripherals,
     })).then(() => {
       dispatch(fetchReservations());
     });
     setStartDate('');
     setEndDate('');
-    setOffice('');
+    setOffice(1);
+    setCheckedStateServices(new Array(9).fill(false));
+    setCheckedStatePeripherals(new Array(9).fill(false));
   };
 
   return (
@@ -49,27 +93,39 @@ export default function Reserve() {
       </div>
 
       <p>Choose your services</p>
-      {servicesList.map((service) => (
-        <>
-          <input type="checkbox" name={service.name} value={service.id} />
+      {servicesList.map((service, index) => (
+        <div key={`service ${service.id}`}>
+          <input
+            type="checkbox"
+            name={service.name}
+            value={service.id}
+            checked={checkedStateServices[index]}
+            onChange={() => handleOnChangeServices(index)}
+          />
           <label htmlFor={service.name}>
             {' '}
             {service.name}
           </label>
           <br />
-        </>
+        </div>
       ))}
 
       <p>Choose your peripherals</p>
-      {peripheralsList.map((peripheral) => (
-        <>
-          <input type="checkbox" name={peripheral.name} value={peripheral.id} />
+      {peripheralsList.map((peripheral, index) => (
+        <div key={`peripheral ${peripheral.id}`}>
+          <input
+            type="checkbox"
+            name={peripheral.name}
+            value={peripheral.id}
+            checked={checkedStatePeripherals[index]}
+            onChange={() => handleOnChangePeripherals(index)}
+          />
           <label htmlFor={peripheral.name}>
             {' '}
             {peripheral.name}
           </label>
           <br />
-        </>
+        </div>
       ))}
 
       <button type="button" onClick={handleClick}>Create Reservation</button>
