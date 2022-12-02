34e541d82d3b0d7c2e1a68dfe193fe7a14292332
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-12-02 15:05:58 +0000

    delete mobile nav component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -42,8 +42,8 @@ export default function NavBar() {
           </button>
         </div>
         <div id="mobile-modal" className={isModalOpen ? 'h-screen w-screen mt-8 md:hidden' : 'hidden md:hidden'}>
-          <div className="w-full md:block md:w-auto" id="navbar-default">
-            <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
+          <div className="w-full h-screen md:block md:w-auto" id="navbar-default">
+            <ul className="flex flex-col p-2 mt-4 gap-4 border-gray-100 rounded-lg bg-gray-50">
               <li>
                 <NavLink to="/" className={({ isActive }) => (isActive ? 'active block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white underline' : 'block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white no-underline')} aria-current="page">
                   Home
