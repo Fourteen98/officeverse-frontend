92fc99c0043664d10c2a958400920f509e7d4cb0
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-12-02 16:08:55 +0000

    add notification reducers to configstore reducers

---
+++ src/components/Notification.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Alert } from '@mui/material';
+import notificationSlice from '../redux/notification/notificationSlice';
+import { useDispatch } from 'react-redux';
+
+function Notification({ type, message }) {
+  const dispatch = useDispatch(state.)
+  return (
+    <div>Notification</div>
+  );
+}
+
+export default Notification;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import officesReducer from './offices/officesSlice';
 import reservationsReducer from './reservations/reservationsSlice';
 import servicesReducer from './services/servicesSlice';
 import peripheralsReducer from './peripherals/peripheralsSlice';
+import notificationSlice from './notification/notificationSlice';
 
 const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ const store = configureStore({
     reservations: reservationsReducer,
     services: servicesReducer,
     peripherals: peripheralsReducer,
+    notification: notificationSlice,
   },
 });
 
