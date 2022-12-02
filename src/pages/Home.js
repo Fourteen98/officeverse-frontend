42d41f27076dd11583696aca6bb52aa355da4189
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-12-02 07:04:28 +0000

    add responsive screen config to tailwind.config.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import Office from '../components/Office';
+// import Office from '../components/Office';
 
 const Home = () => {
   const officeList = useSelector((state) => state.offices.offices);
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx,html}',
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       fontFamily: {
         sans: ['Proxima Nova', 'sans-serif'],
