1a684631cc81cd6163c64b1e507b1108d4ec0e8b
jaferidrees <jaferll@gmail.com>
2022-12-05 12:32:09 +0100

    Add register page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,111 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-param-reassign */
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { createUser } from '../redux/user/userSlice';
 
-function Signup() {
-  return (
-    <div>Signup</div>
+const Signup = () => {
+  const { userInfo, error, success } = useSelector(
+    (state) => state.user,
   );
-}
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // redirect user to login page if registration was successful
+    if (success) navigate('/login');
+    // redirect authenticated user to profile screen
+    if (Object.keys(userInfo).length !== 0) navigate('/user-profile');
+  }, [navigate, userInfo, success]);
+
+  const submitForm = (data) => {
+    if (data.password !== data.confirmPassword) {
+      alert('Password mismatch');
+      return;
+    }
+    // transform email string to lowercase to avoid case sensitivity issues during login
+    data.email = data.email.toLowerCase();
+    dispatch(createUser(data));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(submitForm)}>
+      {error && <p>{error}</p>}
+      <div className="form-group">
+        <label htmlFor="firstName">
+          First Name
+          <input
+            type="text"
+            className="form-input"
+            {...register('firstName')}
+            required
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="lastName">
+          First Name
+          <input
+            type="text"
+            className="form-input"
+            {...register('lastName')}
+            required
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="username">
+          First Name
+          <input
+            type="text"
+            className="form-input"
+            {...register('username')}
+            required
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            className="form-input"
+            {...register('email')}
+            required
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            className="form-input"
+            {...register('password')}
+            required
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">
+          Confirm Password
+          <input
+            type="password"
+            className="form-input"
+            {...register('confirmPassword')}
+            required
+          />
+        </label>
+      </div>
+      <button type="submit" className="button">
+        Register
+      </button>
+    </form>
+  );
+};
 
 export default Signup;
