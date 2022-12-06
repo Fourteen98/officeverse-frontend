0c0f7797504afa60cc3d6220deae2c23728ca5be
Raul Ospina <raminka13@hotmail.com>
2022-12-06 11:08:02 -0500

    Change router for logged user

--- src/App.js
+++ src/App.js
@@ -9,23 +9,45 @@ import OfficeDetails from './pages/OfficeDetails';
 import NewOffice from './pages/NewOffice';
 import MyOffices from './pages/MyOffices';
 import Notification from './components/Notification';
+import Welcome from './pages/Welcome';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   const notification = useSelector((state) => state.notification.notification);
+  const currentUser = useSelector((state) => state.user.user);
 
+  const logged = currentUser.id
+    ? (
+      <Router>
+        {notification && (<Notification type={notification.type} message={notification.message} />)}
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Office />} />
+          <Route path="/reserve" element={<Reserve />} />
+          <Route path="/newoffice" element={<NewOffice />} />
+          <Route path="/myoffices" element={<MyOffices />} />
+          <Route path="/offices/:id" element={<OfficeDetails />} />
+          <Route path="/my-reservations" element={<MyReservations />} />
+        </Routes>
+      </Router>
+    )
+    : (
+      <Router>
+        {notification && (<Notification type={notification.type} message={notification.message} />)}
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<Welcome />} />
+        </Routes>
+      </Router>
+    );
   return (
-    <Router>
-      {notification && (<Notification type={notification.type} message={notification.message} />)}
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Office />} />
-        <Route path="/reserve" element={<Reserve />} />
-        <Route path="/newoffice" element={<NewOffice />} />
-        <Route path="/myoffices" element={<MyOffices />} />
-        <Route path="/offices/:id" element={<OfficeDetails />} />
-        <Route path="/my-reservations" element={<MyReservations />} />
-      </Routes>
-    </Router>
+    <div>
+      { logged }
+    </div>
   );
 }
 
