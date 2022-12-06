62a25442d5cc34ed7a6ed3194fe32d00ac680d54
Fourteen98 <emuhiydeen6@gmail.com>
2022-12-06 09:26:44 +0000

    remove react-multi-carousel and import custom slider

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,18 @@
-rface
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Imageslider = (props) => {
+  const { images } = props;
+  return (
+    <div className="relative h-40 w-full">
+      <img src={images[0]} alt={images[0].name} className="w-full h-full object-cover" />
+    </div>
+  );
+}
+
+// props validation
+Imageslider.prototype = {
+  images: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.object])).isRequired,
+};
+
+export default Imageslider;
--- src/components/SingleOffice.js
+++ src/components/SingleOffice.js
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 import React from 'react';
-import 'react-multi-carousel/lib/styles.css';
 import PropTypes from 'prop-types';
+import Imageslider from './ImageSlider';
 
 const SingleOffice = (props) => {
   const {
@@ -9,17 +9,11 @@ const SingleOffice = (props) => {
   } = props;
 
   images.map((im) => console.log(im));
-  // const officeImages = images.map((image) => (
-  //   <div key={image.id} className="w-full h-96">
-  //     <img src={image.url} alt={image.name} className="w-full h-full object-cover" />
-  //   </div>
-  // ));
-  // <img className="object-fill rounded-lg h-40  min-w-full" src={images} alt="office" />
 
   return (
     <div className="border-2 flex h-54 w-96 md:w-auto lg:w-auto xl:min-w-auto xl:max-w-64 flex-col items-center rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition-transform duration-200 ease-out">
       <div className="relative h-40 w-full">
-        <img src={images[0]} alt={images[0].name} className="w-full h-full object-cover" />
+        <Imageslider images={images} />
       </div>
       <div className="flex flex-col p-2">
         <span className="font-semibold text-xs">
